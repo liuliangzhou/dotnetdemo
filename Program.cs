bb8731bc86bebc0583ffca05b70032c42e579e6e
Zohn <liangzhou.liu@honeywell.com>
2019-04-29 14:41:34 +0800

    update

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace dotnetdemo
 {
@@ -6,7 +7,11 @@ namespace dotnetdemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! "+DateTime.Now.ToString());
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Hello World! " + DateTime.Now.ToString());
+            }
         }
     }
 }
